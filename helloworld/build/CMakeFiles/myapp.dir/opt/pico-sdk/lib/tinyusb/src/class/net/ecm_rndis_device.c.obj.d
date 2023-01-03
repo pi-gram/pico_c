b1882465f870b96a06bd3c5d77badae8bec1a055
tony pi.gram <tony.pigram.me@gmail.com>
2023-01-03 12:42:20 +0000

    initial commit

---
+++ helloworld/build/CMakeFiles/myapp.dir/opt/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c.obj.d
@@ -0,0 +1,29 @@
+CMakeFiles/myapp.dir/opt/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c.obj: \
+ /opt/pico-sdk/lib/tinyusb/src/class/net/ecm_rndis_device.c \
+ /opt/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ /opt/pico-sdk/src/rp2_common/pico_stdio_usb/include/tusb_config.h \
+ /opt/pico-sdk/src/rp2_common/pico_stdio_usb/include/pico/stdio_usb.h \
+ /opt/pico-sdk/src/rp2_common/pico_stdio/include/pico/stdio.h \
+ /opt/pico-sdk/src/common/pico_base/include/pico.h \
+ /opt/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /opt/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/tony/dev/pico/c-code/helloworld/build/generated/pico_base/pico/version.h \
+ /opt/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/tony/dev/pico/c-code/helloworld/build/generated/pico_base/pico/config_autogen.h \
+ /opt/pico-sdk/src/boards/include/boards/pico.h \
+ /opt/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /opt/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /opt/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /opt/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /opt/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /opt/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /opt/pico-sdk/src/common/pico_base/include/pico/error.h
